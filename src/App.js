38256d0a06665e64bc63e8e3e5105f40b15a6633
natalia-ya <sagaidak.nata@gmail.com>
2023-04-28 15:46:26 +0200

    added settings btn

--- src/App.js
+++ src/App.js
@@ -11,10 +11,20 @@ const App = () => {
     <div>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
-          
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                className="text-3x1 p-3 hover:drop-shadow-x1 hover:bg-light-gray text-white"
+                style={{ background: "blue", borderRadius: "50%" }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
         </div>
       </BrowserRouter>
-    </div>>
+    </div>
   );
 };
 
